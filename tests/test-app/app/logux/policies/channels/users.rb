08a05f1c99b45343ef0104329cdf77818d596555
Alex Musayev <alex.musayev@gmail.com>
2021-10-17 12:22:33 +0300

    Metrics/AbcSize

--- tests/test-app/app/logux/policies/channels/users.rb
+++ tests/test-app/app/logux/policies/channels/users.rb
@@ -3,12 +3,14 @@
 module Policies
   module Channels
     class Users < Logux::Policy
+      # rubocop:disable Metrics/AbcSize
       def subscribe?
         raise StandardError, headers['error'] if headers['error'].present?
         params = { user_id: action.channel.split('/')[1] }
         user_id = meta.id.split[1].split(':')[0]
         params[:user_id] == user_id
       end
+      # rubocop:enable Metrics/AbcSize
     end
   end
 end
