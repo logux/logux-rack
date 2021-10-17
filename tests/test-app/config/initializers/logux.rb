dc4c120340f83d19d59886b16f7f537357f085aa
Alex Musayev <alex.musayev@gmail.com>
2021-10-17 12:20:29 +0300

    Style/GuardClause

--- tests/test-app/config/initializers/logux.rb
+++ tests/test-app/config/initializers/logux.rb
@@ -8,14 +8,9 @@ Logux.configure do |config|
   config.supports = '^1.0.0'
 
   config.auth_rule = lambda do |user_id, token, cookie, headers|
-    if headers['error'].present?
-      raise headers['error']
-    elsif token == "#{user_id}:good"
-      return true
-    elsif cookie['token'] == "#{user_id}:good"
-      return true
-    else
-      return false
-    end
+    raise headers['error'] if headers['error'].present?
+    return true if token == "#{user_id}:good"
+    return true if cookie['token'] == "#{user_id}:good"
+    false
   end
 end
