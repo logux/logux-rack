01ec51179b9bfa8dfdd66b38b9b96033b5f5c7fb
Alex Musayev <alex.musayev@gmail.com>
2021-10-17 12:21:11 +0300

    Style/RedundantArgument

--- tests/test-app/app/logux/policies/actions/users.rb
+++ tests/test-app/app/logux/policies/actions/users.rb
@@ -6,7 +6,7 @@ module Policies
       def name?
         raise StandardError, headers['error'] if headers && headers['error'].present?
 
-        user_id = meta.id.split(' ')[1].split(':')[0]
+        user_id = meta.id.split[1].split(':')[0]
         action['payload']['userId'] == user_id
       end
 
--- tests/test-app/app/logux/policies/channels/users.rb
+++ tests/test-app/app/logux/policies/channels/users.rb
@@ -5,9 +5,8 @@ module Policies
     class Users < Logux::Policy
       def subscribe?
         raise StandardError, headers['error'] if headers['error'].present?
-
         params = { user_id: action.channel.split('/')[1] }
-        user_id = meta.id.split(' ')[1].split(':')[0]
+        user_id = meta.id.split[1].split(':')[0]
         params[:user_id] == user_id
       end
     end
