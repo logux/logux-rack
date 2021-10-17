b6a37aad409ce106df25f876f9c657361b8874bb
Alex Musayev <alex.musayev@gmail.com>
2021-10-17 12:43:33 +0300

    Reduce method ABC size

--- tests/test-app/app/logux/actions/users.rb
+++ tests/test-app/app/logux/actions/users.rb
@@ -5,22 +5,29 @@ module Actions
     resend :name, ->(action) { { channels: ["users/#{action['payload']['userId']}"] } }
 
     def name
-      @user = User.find(action['payload']['userId'])
-      old_meta = @user.last_changed ? JSON.parse(@user.last_changed) : nil
-      if @user && Logux::IsFirstOlder.new(old_meta, meta).call
-        @user.update(name: action['payload']['name'], last_changed: meta.to_json)
-      end
+      update_user_name(User.find(action['payload']['userId']))
       respond :processed
     end
 
     def clean
       User.all.each do |user|
         user.update(name: '')
-        send_back({
-                    type: 'users/name',
-                    payload: { userId: user.id.to_s, name: user.name }
-                  })
+
+        send_back(
+          {
+            type: 'users/name',
+            payload: { userId: user.id.to_s, name: user.name }
+          }
+        )
       end
+    end
+
+    private
+
+    def update_user_name(user)
+      old_meta = user.last_changed ? JSON.parse(user.last_changed) : nil
+      return unless Logux::IsFirstOlder.new(old_meta, meta).call
+      user.update!(name: action['payload']['name'], last_changed: meta.to_json)
     end
   end
 end
